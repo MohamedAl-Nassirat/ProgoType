e3aed348832b09fb6668349f52d7ec169a68ac04
Mohamed Al-Nassirat <malnassi@ualberta.ca>
2023-04-16 21:39:16 -0600

    added ability to click on new prompt to get a new prompt, have some more ironing out of bugs to complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useRef, useEffect } from 'react'
-import {allowedKeys} from './Helper'
 import ItemList from './itemList'
 import './App.css'
 import axios from 'axios'
@@ -25,6 +24,34 @@ const random = (array) => {
 	return array[Math.floor(Math.random() * array.length)];
   };
   
+  useEffect(() => {
+	const getNewPrompt = async () => {
+		try {
+		  const response = await axios.get('http://localhost:5001/api/quotes');
+		  const quotes = response.data;
+		  const newQuote = random(quotes);
+		  console.log("new quote:", newQuote);
+		  setQuote(newQuote);
+		  setInput(newQuote.code);
+		  setIndex(0);
+		  setCorrectIndex(0);
+		  setErrorIndex(0);
+		  setCpm(0);
+		  setWpm(0);
+		  setAccuracy(0);
+		  setIsError(false);
+		  outputRef.current.innerHTML = "";
+		  setEnded(false);
+		  setStarted(false);
+		  setDuration(60); // Reset the timer
+		} catch (error) {
+		  console.error('Error fetching quotes:', error);
+		}
+	  };
+  
+	getNewPrompt();
+  }, []);
+
 
 useEffect(() => {
 	const fetchQuotes = async () => {
@@ -48,19 +75,25 @@ useEffect(() => {
 		setEnded(true)
 		setStarted(false)
 		clearInterval(interval)
+		resetTimer()
 	}
 
 	const setTimer = () => {
-		const now = Date.now()
-		const seconds = now + duration * 1000
+		const now = Date.now();
+		const seconds = now + duration * 1000;
+	  
+		if (interval) {
+		  clearInterval(interval);
+		}
+	  
 		interval = setInterval(() => {
-			const secondLeft = Math.round((seconds - Date.now()) / 1000)
-			setDuration(secondLeft)
-			if (secondLeft === 0) {
-				handleEnd()
-			}
-		}, 1000)
-	}
+		  const secondLeft = Math.round((seconds - Date.now()) / 1000);
+		  setDuration(secondLeft);
+		  if (secondLeft === 0) {
+			handleEnd();
+		  }
+		}, 1000);
+	  };
 
 
 	const handleStart = () => {
@@ -74,21 +107,49 @@ useEffect(() => {
 	const handleKeyDown = e => {
 		e.preventDefault()
 		const { key } = e
-		const quoteText = quote.code
+		
+		// ignore Shift Key
+		if (key === "Shift"){
+			return;
+		}
 
+  		// Handle Backspace key
+		if (key === 'Backspace') {
+			if (index > 0) {
+			outputRef.current.innerHTML = outputRef.current.innerHTML.slice(0, -1);
+			setIndex(index - 1);
+			
+			const lastChar = quote.code.charAt(index - 1);
+			
+			if (lastChar === quote.code.charAt(index)) {
+				setIsError(false);
+				setErrorIndex(errorIndex - 1);
+			} else {
+				setIsError(true);
+				setCorrectIndex(correctIndex - 1);
+			}
+			// update the input state to reflect the backspace has been clicked
+			setInput(quote.code.substring(index - 1));
+
+
+			}
+			return;
+		}
+
+		const quoteText = quote.code
 		if (key === quoteText.charAt(index)) {
 			setIndex(index + 1)
 			const currenChar = quoteText.substring(index + 1, index + quoteText.length)
 			setInput(currenChar)
 			setCorrectIndex(correctIndex + 1)
 			setIsError(false)
-      outputRef.current.innerHTML += `<span class="text-correct">${key}</span>`
-		} else {
-			if (allowedKeys.includes(key)) {
-				setErrorIndex(errorIndex + 1)
-				setIsError(true)
-				outputRef.current.innerHTML += `<span class="text-danger">${key}</span>`
-			}
+      		outputRef.current.innerHTML += `<span class="text-correct">${key}</span>`
+		} 
+		else {
+			
+			setErrorIndex(errorIndex + 1)
+			setIsError(true)
+			outputRef.current.innerHTML += `<span class="text-danger">${key}</span>`
 		}
 
 		const timeRemains = ((60 - duration) / 60).toFixed(2)
@@ -106,6 +167,40 @@ useEffect(() => {
 		}
 	}
 
+
+	const resetTimer = () => {
+		if (interval) {
+		  clearInterval(interval);
+		}
+		setDuration(60); // Reset the timer
+	  };
+	  
+
+	const getNewPrompt = async () => {
+		try {
+			resetTimer(); // Reset the timer before fetching a new quote
+			const response = await axios.get('http://localhost:5001/api/quotes');
+			const quotes = response.data;
+			const newQuote = random(quotes);
+			console.log("new quote:", newQuote);
+			setQuote(newQuote);
+			setInput(newQuote.code);
+			setIndex(0);
+			setCorrectIndex(0);
+			setErrorIndex(0);
+			setCpm(0);
+			setWpm(0);
+			setAccuracy(0);
+			setIsError(false);
+			outputRef.current.innerHTML = "";
+		} catch (error) {
+			console.error('Error fetching quotes:', error);
+		}
+	  };
+
+
+
+
 	useEffect(
 		() => {
 			if (ended) localStorage.setItem('wpm', wpm)
@@ -117,36 +212,34 @@ useEffect(() => {
 		if (storedScore) setLastScore(storedScore)
 	}, [])
 
+
+	
+
+	const itemListStyle = (wpm) => {
+		if (wpm > 0 && wpm < 20) return { color: 'white', backgroundColor: '#eb4841' };
+		if (wpm >= 20 && wpm < 40) return { color: 'white', backgroundColor: '#f48847' };
+		if (wpm >= 40 && wpm < 60) return { color: 'white', backgroundColor: '#ffc84a' };
+		if (wpm >= 60 && wpm < 80) return { color: 'white', backgroundColor: '#a6c34c' };
+		if (wpm >= 80) return { color: 'white', backgroundColor: '#4ec04e' };
+		return {};
+	  };
+
+
+
+
+
 	return (
-		<div className="App">
-			<div className="container-fluid pt-4">
-				<div className="row">
-					{/* Left */}
-					<div className="col-sm-6 col-md-2 order-md-0 px-5">
-						<ul className="list-unstyled text-center small">
-							<ItemList
-								name="WPM"
-								data={wpm}
-								style={
-									wpm > 0 && wpm < 20 ? (
-										{ color: 'white', backgroundColor: '#eb4841' }
-									) : wpm >= 20 && wpm < 40 ? (
-										{ color: 'white', backgroundColor: '#f48847' }
-									) : wpm >= 40 && wpm < 60 ? (
-										{ color: 'white', backgroundColor: '#ffc84a' }
-									) : wpm >= 60 && wpm < 80 ? (
-										{ color: 'white', backgroundColor: '#a6c34c' }
-									) : wpm >= 80 ? (
-										{ color: 'white', backgroundColor: '#4ec04e' }
-									) : (
-										{}
-									)
-								}
-							/>
-							<ItemList name="CPM" data={cpm} />
-							<ItemList name="Last Score" data={lastScore} />
-						</ul>
-					</div>
+    <div className="App">
+      <div className="container-fluid pt-4">
+        <div className="row">
+          {/* Left */}
+          <div className="col-sm-6 col-md-2 order-md-0 px-5">
+            <ul className="list-unstyled text-center small">
+              <ItemList name="WPM" data={wpm} style={itemListStyle(wpm)} />
+              <ItemList name="CPM" data={cpm} />
+              <ItemList name="Last Score" data={lastScore} />
+            </ul>
+          </div>
 					{/* Body */}
 					<div className="col-sm-12 col-md-8 order-md-1">
 						<div className="container">
@@ -155,6 +248,7 @@ useEffect(() => {
                             <p className="lead">
                               Increase your programming typing speed.
                             </p>
+							<button className="newPrompt" onClick={getNewPrompt}>New Prompt</button>
                           </div>
 							{ended ? (
 								<div className="quotes">
